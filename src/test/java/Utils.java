0dafbcb3a3a8598c1684d34220f864fdc38c0561
aniksur034 <aniksur030@gmail.com>
2025-05-05 14:02:39 +0600

    Save token in config properties

---
+++ src/test/java/Utils.java
@@ -0,0 +1,14 @@
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class Utils {
+    public static void setEnvVar(String key, String value) throws ConfigurationException {
+        PropertiesConfiguration config = new PropertiesConfiguration("src/test/resources/config.properties");
+
+        //PropertiesConfiguration config = new PropertiesConfiguration(".src/test/resources/config.properties");
+        config.setProperty(key, value);
+        config.save();
+
+
+    }
+}
--- src/test/java/myRestAssured.java
+++ src/test/java/myRestAssured.java
@@ -1,12 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.apache.commons.configuration.ConfigurationException;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class myRestAssured {
 @Test
-    public void doLogin(){
+    public void doLogin() throws ConfigurationException {
         RestAssured.baseURI = "https://dailyfinanceapi.roadtocareer.net";
         Response res = given().contentType("application/json")
                 .body("{\n" +
@@ -15,6 +17,11 @@ public class myRestAssured {
                         "}")
                 .when().post("/api/auth/login");
 
-        System.out.println("Response: " + res.asString());
+        //System.out.println("Response: " + res.asString());
+    JsonPath jsonObject = res.jsonPath();
+    String token = jsonObject.get("token");
+    System.out.println("Token: " + token);
+    Utils.setEnvVar("token", token);
+
     }
 }
